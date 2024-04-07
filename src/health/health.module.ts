77beb472b4a987353b7a4d2abbc2b340bc61c2e4
Masato Naka <masatonaka1989@gmail.com>
2024-04-07 19:47:14 +0900

    use relative path

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { HealthController } from './health.controller';
 import { TerminusModule } from '@nestjs/terminus';
-import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [TerminusModule, PrismaModule],
